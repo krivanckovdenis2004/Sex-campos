205393c2170587190abfd39a7df141760e3abd69
krivanckovdenis2004 <krivanckovdenis2004@icloud.com>
2026-05-20 00:27:24 +0300

    Add files via upload

---
+++ firebase.js
@@ -0,0 +1,26 @@
+
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import {
+  getFirestore,
+  collection,
+  addDoc,
+  getDocs
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDELdrcdlFv6OAWOzPLEBYFOzcQLcK_SgM",
+  authDomain: "sex-campus.firebaseapp.com",
+  projectId: "sex-campus",
+  storageBucket: "sex-campus.firebasestorage.app",
+  messagingSenderId: "1021649767987",
+  appId: "1:1021649767987:web:ccc0b366a159d689e8dd46",
+  measurementId: "G-ZP8X7YRLKW"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+window.firebaseDB = db;
+window.firebaseCollection = collection;
+window.firebaseAddDoc = addDoc;
+window.firebaseGetDocs = getDocs;
